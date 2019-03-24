97cb26be51adb0f4cab4fa30eb4526b69488b317
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-03-23 22:37:33 -0400

    added input sanitisation in server

--- server/server.c
+++ server/server.c
@@ -5,16 +5,33 @@
 #include <arpa/inet.h>
 
 
-int main(){
+int main(int argc, char** argv){
 	int list_s;
+	short int port;
 	struct sockaddr_in servaddr;
+	char* endptr;
+
+	//input sanitisation
+	if (argc!=2){
+		errno = EINVAL;
+		error("usage: ./server PORT_NO\n");
+	}
+	else{
+		port = strtol(argv[1], &endptr, 0);
+		if (*endptr){
+			errno = EINVAL;
+			error("Invalid port number supplied!");
+		}
+	}
+
 
 	if( (list_s = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ){
 		error("Unable to create listening socket");
 	}
 
 	//assumption : successful creation of socket
-
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons();
 
 	return 0;
 }
